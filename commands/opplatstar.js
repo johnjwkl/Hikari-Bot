60408a16766416978efd71ab8bb09aa44e9cfe08
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-04 19:40:17 +0800

    yee

---
+++ commands/opplatstar.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'opplatstar',
+	description: 'opplatstar',
+	execute(message, args) {
+		message.channel.send("https://pic4.zhimg.com/80/v2-ad86e2aa70127d699307f75ded27cd63_1440w.jpg")
+	},
+};

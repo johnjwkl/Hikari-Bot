7b173d3ef434522e245ecd8dd2250eb152af2f6c
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-04 19:35:26 +0800

    yee

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'help',
 	description: 'help',
 	execute(message, args) {
-		message.channel.send("```歡迎使用小光的機器人喔 =]\n這機器人又很多指令等著你去探索喔\nh!ping 自動回答 pong\nh!fortune 查看今天的運氣\nh!nezuko 禰豆子跑步awa\nh!fgodraw 讓你測試你484歐洲人喔```")
+		message.channel.send("```歡迎使用小光的機器人喔 =]\n這機器人又很多指令等著你去探索喔\nh!ping 自動回答 pong\nh!fortune 查看今天的運氣\nh!nezuko 禰豆子跑步awa\nh!fgodraw 讓你測試你484歐洲人喔\nh!ok 簡單敷衍別人最佳辦法```")
 	},
 };
---
+++ commands/no.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'no',
+	description: 'no',
+	execute(message, args) {
+		message.channel.send("https://i.imgur.com/R46wtal.png")
+		message.channel.send("可是我拒絕")
+	},
+};

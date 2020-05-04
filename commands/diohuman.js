f72ada2c627ffa4026f369129da519e03bdb8d9b
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-04 19:45:49 +0800

    yee

---
+++ commands/diohuman.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'diohuman',
+	description: 'diohuman',
+	execute(message, args) {
+		message.channel.send("https://img.moegirl.org/common/a/a6/Jojoios-4.jpg")
+	},
+};
---
+++ commands/nasty.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'nasty',
+	description: 'nasty',
+	execute(message, args) {
+		message.channel.send("https://memes.tw/user-template/ed5e006533ec99cbeb3e645a3ab45572.png")
+	},
+};

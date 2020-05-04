70741e32f0eca5c43c1c985662066019f000e8cd
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-04 21:23:39 +0800

    yee

--- commands/ban.js
+++ commands/ban.js
@@ -7,9 +7,8 @@ module.exports = {
 			banMember.ban().then((banMember)=>{
 				message.channel.send(banMember.displayName + "這垃圾已經被ban掉咯")
 				client.sendMessage(banMember, "可憐哪被ban掉了")
+			}).catch(()=>{
+				message.channel.send("沒管理員ban你ma呢");
 			})
-		else {
-			message.channel.send("沒管理員ban你ma呢")
-		}
 		}
 };

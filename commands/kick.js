4a797b27b9df9fee8bc246fceb6fc35bb60b387b
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-04 21:32:23 +0800

    yee

--- commands/kick.js
+++ commands/kick.js
@@ -4,7 +4,7 @@ module.exports = {
 	execute(message, args) {
 		if (message.member.hasPermission("ADMINISTRATOR")){
 			var kickMember = message.mentions.members.first();
-			banMember.ban().then((kickMember)=>{
+			kickMember.kick().then((kickMember)=>{
 				message.channel.send(kickMember.displayName + "這垃圾已經被踢屁股咯")
 			}).catch(()=>{
 				message.channel.send("沒管理員踢你ma屁股哦");

a08f10b3cbbb648b4a9c6db48ff7bffb48b0aa73
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-04 19:47:01 +0800

    yee

--- commands/jojohelp.js
+++ commands/jojohelp.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'jojohelp',
 	description: 'jojohelp',
 	execute(message, args) {
-		message.channel.send("```JOJO梗圖專區awa\nh!no 可是我拒絕\nh!opplatstar 快點用你無敵的白金之星想想辦法\nh!我不做人啦JOJO```")
+		message.channel.send("```JOJO梗圖專區awa\nh!no 可是我拒絕\nh!opplatstar 快點用你無敵的白金之星想想辦法\nh!diohuman我不做人啦JOJO```")
 	},
 };

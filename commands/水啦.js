1b8fd6826cae861589833e5463676f1ee17737b5
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-05 15:10:47 +0800

    yee

---
+++ commands/水啦.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'explosion',
+	description: 'explosion',
+	execute(message, args){
+		message.channel.send('https://truth.bahamut.com.tw/s01/202003/48a12066c5e7bb7a640079e1df2a9696.JPG')
+    }
+}

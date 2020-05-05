532858605552f841b5d120fc238baedec8aebb10
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-05 15:06:23 +0800

    yee

--- commands/fgo.js
+++ commands/fgo.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'fgodraw',
 	execute(message, args){
 		var fgo = Math.round(Math.random()*99)
-		if (fgo == 1){
+		if (fgo == 3){
 			message.channel.send("恭喜你咯 5*卡 出現了awa")
 		}
 		else {

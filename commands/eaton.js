49d6d39898a7d84993c73d229feeedf489e3aa35
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-07 22:43:18 +0800

    yee

--- commands/eaton.js
+++ commands/eaton.js
@@ -2,10 +2,14 @@ module.exports = {
 	name: 'eaton',
 	description: 'eaton',
 	execute(message, args) {
-		message.channel.send('Eaton = Potato confirmed',{
-			files: [
-			"./eaton.png"
-			]
-		})
+		if(!message.member.roles.find(role => role.name === "School Assholes"))
+			return message.channel.send("Access denied");
+		var eaton = Math.round(Math.random()*2+1);
+		if (eaton == 1 ){
+			return message.channel.send("Eaton = Potato confirmed", files:{["./res/eaton.png"]});
+		}
+		if (eaton == 2){
+			return message.channel.send("Eaton now dancing with the coffin team~ ", files:{["./res/eatoncoffin.jpeg"]});
+		}
 	}
 };

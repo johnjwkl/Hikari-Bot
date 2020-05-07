74b01fffc30a24947dd3eeeeabbdcb5de32736db
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-07 22:57:53 +0800

    yee

--- commands/fgo.js
+++ commands/fgo.js
@@ -4,7 +4,7 @@ module.exports = {
 	execute(message, args){
 		var fgo = Math.round(Math.random()*99)
 		if (fgo == 3){
-			message.channel.send("恭喜你咯 5*卡 出現了awa")
+			message.channel.send("恭喜你咯 5*卡 出現了awa", {files: ['./res/padoru.gif']})
 		}
 		else {
 			message.channel.send("果然啊 你還是沒能逃出非洲人的命運")

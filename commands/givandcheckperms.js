0983ca516fe7d69f04e2f684401174856e832afb
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-09 12:55:23 +0800

    yee

--- commands/givandcheckperms.js
+++ commands/givandcheckperms.js
@@ -5,7 +5,7 @@ module.exports = {
 		message.delete(10000);
 		let myRole = message.guild.roles.find(role => role.name === "Luminous Bot");
 		let member = message.mentions.members.first();
-		if (!client.guild.me.hasPermssion("ADMINISTRATOR"))
+		if (!guild.me.hasPermssion("ADMINISTRATOR"))
 			return console.log("No admin perms");
 		if (!myRole) {
 			message.guild.createRole({

